a59072b55201805bcd1155c34e29f6379ab886fa
Stellareign <tuguran@mail.ru>
2022-10-19 21:38:31 +0300

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 3:");
-        var classLP = 23;
-        var classAS = 27;
-        var classEA = 30;
-        var sheats = 480;
-        var sheatsForChild = sheats / (classLP + classAS + classEA);
+        int classLP = 23;
+        int classAS = 27;
+        int classEA = 30;
+        int sheats = 480;
+        double sheatsForChild = sheats / (classLP + classAS + classEA);
         System.out.println("На каждого ученика рассчитано " + sheatsForChild + " листов бумаги");
 
 
