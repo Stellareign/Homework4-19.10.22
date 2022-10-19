d6d4644a9d160e1897961eab7a07d0a5844d3e3f
Stellareign <tuguran@mail.ru>
2022-10-19 22:06:09 +0300

    Задача 4

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задание 3:");
-        int classLP = 23;
-        int classAS = 27;
-        int classEA = 30;
-        int sheats = 480;
-        double sheatsForChild = sheats / (classLP + classAS + classEA);
-        System.out.println("На каждого ученика рассчитано " + sheatsForChild + " листов бумаги");
+        System.out.println("Задание 4:");
+        int efficiencyTime = 2;
+       int efficiencyBottles = 16;
+       int timeMinutes = 20;
+       int dayMinutes = 24 * 60;
+       int threeDayMinutes = 3 * 24 * 60;
+       int monthMinutes = threeDayMinutes * 10;
+       float in20MinutesBottles = timeMinutes / efficiencyTime * efficiencyBottles;
+        System.out.println("За " + timeMinutes + " минут машина произвела бутылок " + in20MinutesBottles + " штук.");
+        float inDayBottles = dayMinutes / efficiencyTime * efficiencyBottles;
+        System.out.println("За день машина произвела бутылок " + inDayBottles + " штук.");
+        float inThreeDayBottles = threeDayMinutes / efficiencyTime * efficiencyBottles;
+        System.out.println("За три дня машина произвела бутылок " + inThreeDayBottles + " штук.");
+        float inMonthBottles = monthMinutes / efficiencyTime * efficiencyBottles;
+        System.out.println("За месяц машина произвела бутылок " + inMonthBottles + " штук.");
+
+
 
 
 
