6f6a3d186bf98c3386476dbd824420f85b9e332f
Stellareign <tuguran@mail.ru>
2022-10-19 21:02:03 +0300

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Задача 1:");
+        int ab = 25;
+        System.out.println("Значение переменной ab с типом int равно " + ab);
+        byte cd = 127;
+        System.out.println("Значение переменной cd с типом byte равно " + cd);
+        short ef = 2396;
+        System.out.println("Значение переменной ef с типом short равно " + ef);
+        long gh = 2369492;
+        System.out.println("Значение переменной gh с типом long равно " + gh);
+        float ij = 2.3f;
+        System.out.println("Значение переменной ij с типом float равно " + ij);
+        double kl = 126.36;
+        System.out.println("Значение переменной kl с типом double равно " + kl);
+
+    }
+}
