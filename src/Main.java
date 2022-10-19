5799231ec2959722dc937fcfe393b389e08e16e9
Stellareign <tuguran@mail.ru>
2022-10-19 21:35:48 +0300

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задание 2:");
+        System.out.println("Задание 3:");
+        var classLP = 23;
+        var classAS = 27;
+        var classEA = 30;
+        var sheats = 480;
+        var sheatsForChild = sheats / (classLP + classAS + classEA);
+        System.out.println("На каждого ученика рассчитано " + sheatsForChild + " листов бумаги");
 
-     float one = 27.12f;
-        System.out.println(one);
-     long two = 987678965549L;
-        System.out.println(two);
-     byte three = 2;
-        System.out.println(three);
-     short four = 786;
-        System.out.println(four);
-     double five = 36.5;
-     boolean isFive = five < 35;
-        System.out.println(isFive);
-     int six = 569;
-        System.out.println(six);
-     int seven = -159;
-        System.out.println(seven);
-     int eight = 27897;
-        System.out.println(eight);
-     short nine = 67;
-        System.out.println(nine);
+
 
 
     }
