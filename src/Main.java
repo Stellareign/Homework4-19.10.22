4e921f33767c462bf1ad1d7ca5d2946f0e83f97e
Stellareign <tuguran@mail.ru>
2022-10-20 21:29:36 +0300

    Задача 8

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 8:");
-    double salaryMashaRub = 67760.0;
+    double salaryMashaRub = 67_760.0;
         double salaryMashaIncrease = salaryMashaRub * 1.10;
         double YearSalaryMasha = salaryMashaRub * 12;
         double YearSalaryMashaIncrease = salaryMashaIncrease * 12;
         double increaseAnnualIncomeMasha = YearSalaryMashaIncrease - YearSalaryMasha;
         System.out.println("Маша теперь в месяц получает " + salaryMashaIncrease + " годовой доход вырос на " + increaseAnnualIncomeMasha + " рублей");
-    double salaryDenisRub = 83690.0;
+    double salaryDenisRub = 83_690.0;
         double salaryDenisIncrease = salaryDenisRub * 1.10;
         double salaryYearDenis = salaryDenisRub * 12;
         double salaryYearDenisIncrease = salaryDenisIncrease * 12;
         double increaseAnnualIncomeDenis = salaryYearDenisIncrease - salaryYearDenis;
         System.out.println("Денис теперь в месяц получает " + salaryDenisIncrease + " годовой доход вырос на " + increaseAnnualIncomeDenis  + " рублей");
-    double salaryKristinaRub = 76230.0;
+    double salaryKristinaRub = 76_230.0;
         double salaryKristinaIncrease = salaryKristinaRub * 1.10;
         double salaryYearKristina = salaryKristinaRub + 12;
         double salaryYearKristinaIncrease = salaryKristinaIncrease * 12;
